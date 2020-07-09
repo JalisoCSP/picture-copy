7d550c2f37a0297df9108b40326db3322bb70ef0
Craig Petterson <cspetterson@hey.com>
2020-07-09 20:40:47 +0100

    Add tailwind with initial design thought

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+
+  theme: {
+    extend: {},
+  },
+  variants: {}
+}
